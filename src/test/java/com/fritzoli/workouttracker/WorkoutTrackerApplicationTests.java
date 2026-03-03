1f7839f1d860adb7c104a6be4fd6ab73183bd104
Oliver Fritz <fritzolli05@gmail.com>
2026-03-03 09:29:08 +0100

    initial commit

---
+++ src/test/java/com/fritzoli/workouttracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fritzoli.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
