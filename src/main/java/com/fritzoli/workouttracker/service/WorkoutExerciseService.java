c835dd9a4ec087e2d105c657fa21f888d56ef402
Oliver Fritz <fritzolli05@gmail.com>
2026-03-25 22:32:21 +0100

    added endpoints for workoutExercise

--- src/main/java/com/fritzoli/workouttracker/service/WorkoutExerciseService.java
+++ src/main/java/com/fritzoli/workouttracker/service/WorkoutExerciseService.java
@@ -2,6 +2,8 @@ package com.fritzoli.workouttracker.service;
 
 import com.fritzoli.workouttracker.dto.request.WorkoutExerciseRequest;
 import com.fritzoli.workouttracker.dto.response.WorkoutExerciseResponse;
+import com.fritzoli.workouttracker.exception.custom.ResourceNotFoundException;
+import com.fritzoli.workouttracker.exception.custom.UserNotAuthenticatedException;
 import com.fritzoli.workouttracker.model.user.IUser;
 import com.fritzoli.workouttracker.model.workout.WorkoutExercise;
 import com.fritzoli.workouttracker.repository.workout.IWorkoutExerciseRepository;
@@ -33,6 +35,35 @@ public class WorkoutExerciseService {
         return new WorkoutExerciseResponse(res.getId(),
                 exercise.getId(),
                 workout.getId(),
+                res.getSets(),
+                res.getReps(),
+                res.getRpe(),
+                res.getCreationdate());
+    }
+
+    public void deleteWorkoutExercise(String id, IUser userDetails) {
+        var we = repository.findById(id);
+
+        if (we.isEmpty())
+            throw new ResourceNotFoundException("Workout exercise not found");
+
+        // Prevents other users from deleting exercises
+        if (!we.get().getWorkout().getUser().getId().equals(userDetails.getId()))
+            throw new UserNotAuthenticatedException("Wrong workout exercise id");
+
+        repository.delete(we.get());
+    }
+
+    public WorkoutExerciseResponse getWorkoutExerciseById(String workoutExerciseId, IUser userDetails) {
+        var we = repository.findById(workoutExerciseId);
+
+        if (we.isEmpty() || !we.get().getWorkout().getUser().getId().equals(userDetails.getId()))
+            throw new ResourceNotFoundException("Exercise not found");
+        var res = we.get();
+
+        return new WorkoutExerciseResponse(res.getId(),
+                res.getExercise().getId(),
+                res.getWorkout().getId(),
                 res.getSets(),
                 res.getReps(),
                 res.getRpe(),
